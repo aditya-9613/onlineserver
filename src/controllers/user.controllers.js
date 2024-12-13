305542217209d37de41c01ae76d52881a8f963da
aditya-9613 <155987958+aditya-9613@users.noreply.github.com>
2024-12-13 17:47:08 +0000

    full comprlete integration

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -5,7 +5,17 @@ import { asyncHandler } from '../utils/asyncHandler.js'
 
 const userRegister = asyncHandler(async (req,res) => {
     
-    
+    const {user} = req.body
+
+    if (!user) {
+        throw new ApiError(400,'Required Fields')
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200,user,'User Available')
+        )
 
 })
 
